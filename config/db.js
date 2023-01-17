2379caa6f2c00ec775fa9b9d817b5d1aa9fddff3
Jason <gt07891@greattree.com.tw>
2023-01-17 09:48:46 +0800

    fix DB連線範例

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool.promise();
